e386513b7a102a0375356f9d3b5901c9a16e2368
Allison Lyon <alyoness1@gmail.com>
2016-06-25 16:02:27 -0700

    updated models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -19,6 +19,9 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    def do_nothing(self):
+        pass
+
 
 @python_2_unicode_compatible
 class Choice(models.Model):
