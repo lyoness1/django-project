bd68bc16318d1409fb3b2a1fe2790310dcf5230a
Allison Lyon <alyoness1@gmail.com>
2016-06-25 19:11:18 -0700

    index view added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    """Renders the home page of the polls app."""
+
+    # get the most recent 5 questions from the db
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # load the index template from polls/templates/polls/
+    template = loader.get_template('polls/index.html')
+
+    # set up context as dictionary with variables to pass to render view
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    # render the template with the context as a response to the request
+    return HttpResponse(template.render(context, request))
+
+    # can also use (negates the need for 'loader' and 'HttpResponse' objects):
+    # return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
