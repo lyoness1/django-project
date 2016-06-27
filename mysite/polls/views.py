f6c72279521ba7aa4fa92c28d2485d45747eb2e6
Allison Lyon <alyoness1@gmail.com>
2016-06-26 17:40:20 -0700

    voting enabled

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
 
 
 def index(request):
@@ -39,18 +40,43 @@ def detail(request, question_id):
 
     # could also use get() and riase Http404:
 
-    # from django.shortcuts import get_object_or_404
     # question = get_object_or_404(Question, pk=question_id)
     # return render(request, 'polls/detail.html', {'question': question})
 
-    # There’s also a get_list_or_404() function, which works just as 
-    # get_object_or_404() – except using filter() instead of get(). 
-    # It raises Http404 if the list is empty.
+    # There’s also a get_list_or_404() function, which works just as
+    # get_object_or_404() – except using filter() instead of get().
+    # It raises Http404 if the list is empty
 
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    """Handles submitting a vote for a question from the details view"""
+
+    # get Question object from db
+    question = get_object_or_404(Question, pk=question_id)
+
+    # get choice voted for or insist the user votes again with error msg
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form (details view)
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    # increment votes for choice, commit to db, and redirect to results view
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(
+            reverse('polls;results', args=(question.id,))
+        )
+
+
+
+
+
